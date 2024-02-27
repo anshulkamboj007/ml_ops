1d8fe7c646c5b5a03ec20fef5f88e367b24a4ec8
anshulsinghkamboj <anshulsinghkamboj@gmail.com>
2024-02-27 12:40:12 +0530

    o6

---
+++ src/evaluation.py
@@ -0,0 +1,46 @@
+import logging
+from abc import ABC,abstractmethod
+import numpy as np
+from sklearn.metrics import mean_squared_error,r2_score
+
+class Evaluation(ABC):
+    """
+    """
+    @abstractmethod
+    def calculate_scores(self,y_true:np.ndarray,y_pred:np.ndarray):
+        """
+        """
+        pass
+
+class MSE(Evaluation):
+    def calculate_scores(self,y_true:np.ndarray,y_pred:np.ndarray):
+        try:
+            logging.info("calculating MSE")
+            mse=mean_squared_error(y_true,y_pred)
+            logging.info("MSE: {}".format(mse))
+            return mse
+        except Exception as e:
+            logging.error("error in calculation MSE: {}".format(e))
+            raise e
+        
+class R2(Evaluation):
+    def calculate_scores(self,y_true:np.ndarray,y_pred:np.ndarray):
+        try:
+            logging.info("calculating MSE")
+            r2=r2_score(y_true,y_pred)
+            logging.info("r2: {}".format(r2))
+            return r2
+        except Exception as e:
+            logging.error("error in calculation r2: {}".format(e))
+            raise e
+        
+class RMSE(Evaluation):
+    def calculate_scores(self,y_true:np.ndarray,y_pred:np.ndarray):
+        try:
+            logging.info("calculating MSE")
+            rmse=mean_squared_error(y_true,y_pred,squared=False)
+            logging.info("MSE: {}".format(rmse))
+            return rmse
+        except Exception as e:
+            logging.error("error in calculation RMSE: {}".format(e))
+            raise e
--- steps/model_train.py
+++ steps/model_train.py
@@ -1,8 +1,30 @@
 import logging
 import pandas as pd
 from zenml import step
+from src.model_dev import LinearRegressionModel
+from sklearn.base import RegressorMixin
+from .config import ModelNameConfig
 
 @step
+def train_model(X_train:pd.DataFrame,X_test:pd.DataFrame,
+                y_train:pd.DataFrame,y_test:pd.DataFrame)->RegressorMixin:
+    """
+    trains model on ingested data
 
-def train_model(df:pd.DataFrame)->None:
-    pass
+    Args:
+    X_train:pd.DataFrame,X_test:pd.DataFrame,
+    y_train:pd.DataFrame,y_test:pd.DataFrame
+
+    """
+    try:
+        model=None
+
+        if config.model_name=="LinearRegression":
+            model=LinearRegressionModel()
+            trained_model=model.train(X_train,y_train)
+            return trained_model
+        else: 
+            raise ValueError("model {} not supported".format(config.model_name))
+    except Exception as e:
+        logging.error("eror in traing model;{}".format(e))
+        raise e
